5041379b68ccba04de96258505e3af1ba688850a
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2024-03-31 00:32:18 +0530

    Docker compose added

---
+++ README.Docker.md
@@ -0,0 +1,9 @@
+# In-house-auth
+
+## Run Locally 
+
+```
+docker compose up
+```
+
+Make sure to have docker daemon running and install mongodb compass to see the database running at `localhost:27017`
--- src/config/db_connection_handler.rs
+++ src/config/db_connection_handler.rs
@@ -3,13 +3,13 @@ use mongodb::{
     options::{ClientOptions, ResolverConfig},
     Client,
 };
-// use std::env;
+use std::env;
 use std::error::Error;
 
 pub async fn mongo_connection_handler() -> Result<Client, Box<dyn Error>> {
     // Load the MongoDB connection string from an environment variable:
-    let client_uri =
-        "mongodb://admin:admin@localhost:27017/?directConnection=true&retryWrites=true&w=majority";
+    let client_uri =  env::var("MONGO_URI").unwrap_or("mongodb://admin:admin@localhost:27017/?directConnection=true&retryWrites=true&w=majority".to_string());
+        
 
     // A Client is needed to connect to MongoDB:
     // An extra line of code to work around a DNS issue on Windows:
