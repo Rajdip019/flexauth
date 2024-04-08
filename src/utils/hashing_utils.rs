c0541e3dbd3df31aa7b009d8c35e24812aa32acc
Debajyoti Saha <debajyotisaha14@gmail.com>
2024-04-09 01:29:20 +0530

    Stored Encrypted Pass and other infos to DB

--- src/utils/hashing_utils.rs
+++ src/utils/hashing_utils.rs
@@ -1,9 +1,6 @@
 use aes_gcm::{aead::Aead, AeadCore, Aes256Gcm, Key, KeyInit};
 use argon2::{
-    password_hash::{
-        rand_core::OsRng,
-        PasswordHasher, SaltString,
-    },
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
     Argon2,
 };
 use sha256::digest;
@@ -44,10 +41,22 @@ pub fn verify_password(password: &str, salt: &str, hash: &str) -> bool {
 pub fn create_dek() -> String {
     let key = Aes256Gcm::generate_key(OsRng);
     // convert the key to hex string
-    let hex_key = key.iter().map(|b| format!("{:02x}", b)).collect::<String>().chars().take(32).collect::<String>();
+    let hex_key = key
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>()
+        .chars()
+        .take(32)
+        .collect::<String>();
     let iv = Aes256Gcm::generate_nonce(&mut OsRng);
     // convert the iv to hex string
-    let hex_iv = iv.iter().map(|b| format!("{:02x}", b)).collect::<String>().chars().take(12).collect::<String>();
+    let hex_iv = iv
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>()
+        .chars()
+        .take(12)
+        .collect::<String>();
     // connect the key and iv with . between them
     let key_iv = format!("{}.{}", hex_key, hex_iv);
     return key_iv;
@@ -58,7 +67,9 @@ pub fn encrypt_data(data: &str, key_iv: &str) -> String {
     let key_iv_vec: Vec<&str> = key_iv.split('.').collect();
     let key_buff = Key::<Aes256Gcm>::from_slice(key_iv_vec[0].as_bytes().try_into().unwrap());
     let cipher = Aes256Gcm::new(key_buff);
-    let cipher_text = cipher.encrypt(key_iv_vec[1].as_bytes().into(), data.as_ref()).unwrap();
+    let cipher_text = cipher
+        .encrypt(key_iv_vec[1].as_bytes().into(), data.as_ref())
+        .unwrap();
     // convert the cipher_text to string
     return hex::encode(cipher_text);
 }
@@ -70,6 +81,8 @@ pub fn decrypt_data(cipher_text: &str, key_iv: &str) -> String {
     let key_iv_vec: Vec<&str> = key_iv.split('.').collect();
     let key_buff = Key::<Aes256Gcm>::from_slice(key_iv_vec[0].as_bytes().try_into().unwrap());
     let cipher = Aes256Gcm::new(key_buff);
-    let data = cipher.decrypt(key_iv_vec[1].as_bytes().into(), cipher_text.as_ref()).unwrap();
+    let data = cipher
+        .decrypt(key_iv_vec[1].as_bytes().into(), cipher_text.as_ref())
+        .unwrap();
     return String::from_utf8(data).unwrap();
 }
