894c28de5654d8b9608ec0cffbc317417c8e5fad
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2024-04-19 02:02:41 +0530

    Added jwt verification

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .route("/", get(root_handler))
         .merge(routes::health_check_routes::routes())
         .merge(routes::user_routes::routes(State(app_state.clone())))
-        .merge(routes::password_routes::routes(State(app_state)))
+        .merge(routes::password_routes::routes(State(app_state.clone())))
+        .merge(routes::session_routes::routes())
         .layer(middleware::map_response(main_response_mapper));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
