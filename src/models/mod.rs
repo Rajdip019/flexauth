cf4a0ae9eae6b47841fee6fd963875866da3067a
Debajyoti Saha <debajyotisaha14@gmail.com>
2024-04-21 01:08:41 +0530

    Added Auth Route, handler, model

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,5 +1,6 @@
-pub mod user_model;
+pub mod auth_model;
 pub mod dek_model;
-pub mod password_model;
 pub mod forget_password_req_model;
+pub mod password_model;
 pub mod session_model;
+pub mod user_model;
--- src/routes/user_routes.rs
+++ src/routes/user_routes.rs
@@ -6,17 +6,14 @@ use axum::{
 
 use crate::{
     handlers::user_handler::{
-        delete_user_handler, get_all_users_handler, get_user_handler, signin_handler,
-        signup_handler, toggle_user_activation_status, update_user_handler,
-        update_user_role_handler,
+        delete_user_handler, get_all_users_handler, get_user_handler,
+        toggle_user_activation_status, update_user_handler, update_user_role_handler,
     },
     AppState,
 };
 
 pub fn routes(State(state): State<AppState>) -> Router {
     Router::new()
-        .route("/signup", post(signup_handler))
-        .route("/signin", post(signin_handler))
         .route("/get-all-users", get(get_all_users_handler))
         .route("/get-user", post(get_user_handler))
         .route("/update-user", post(update_user_handler))
