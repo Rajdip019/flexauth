27e4afc549b52810b0251db32c627e02086e6a84
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2024-05-03 23:38:35 +0530

    changed form is_active to is_revoked

--- src/models/session_model.rs
+++ src/models/session_model.rs
@@ -1,6 +1,17 @@
+use bson::DateTime;
 use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Debug, Clone, Serialize)]
-pub struct VerifyJwt {
+pub struct VerifySession {
     pub token: String,
+}
+
+#[derive(Deserialize, Debug, Clone, Serialize)]
+pub struct SessionResponse {
+    pub uid : String,
+    pub email : String,
+    pub user_agent : String,
+    pub is_revoked : bool,
+    pub created_at : DateTime,
+    pub updated_at : DateTime,
 }
