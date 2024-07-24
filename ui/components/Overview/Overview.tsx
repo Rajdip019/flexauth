6cc04e53429739b39cc37abc49db511399dd6a96
Debajyoti14 <debajyotisaha14@gmail.com>
2024-07-24 21:40:06 +0530

    Dashboard Done

--- ui/components/Overview/Overview.tsx
+++ ui/components/Overview/Overview.tsx
@@ -1,17 +1,135 @@
 "use client";
 import React, { useEffect, useState } from 'react'
 import { Loader } from '../custom/Loader';
-import { IUser } from '@/interfaces/IUser';
-import { MdOutlineConstruction } from "react-icons/md";
+import { IOverview } from '@/interfaces/IOverview';
+import { DonutChartStats } from '../custom/DonutChartForStats';
+import { ChartConfig } from '../ui/chart';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { FaUsersSlash } from 'react-icons/fa';
+import { ChartPie } from '../custom/PieChart';
 
 const Overview = () => {
-    const [users, setUsers] = useState([] as IUser[])
+    const [overview, setOverview] = useState<IOverview | null>(null)
     const [loading, setLoading] = useState(true)
 
-    const getAllUsers = async () => {
+    const userChartData = [
+        { name: "active", count: overview?.active_user_count, fill: "var(--color-active)" },
+        { name: "inactive", count: overview?.inactive_user_count, fill: "var(--color-inactive)" },
+    ]
+
+    const userChartConfig = {
+        user: {
+            label: "Users",
+        },
+        active: {
+            label: "Active",
+            color: "hsl(var(--chart-1-1))",
+        },
+        inactive: {
+            label: "Inactive",
+            color: "hsl(var(--chart-2-1))",
+        },
+    } satisfies ChartConfig
+
+    const sessionChartData = [
+        { name: "active", count: overview?.active_session_count, fill: "var(--color-active)" },
+        { name: "revoked", count: overview?.revoked_session_count, fill: "var(--color-revoked)" },
+    ]
+
+    const sessionChartConfig = {
+        session: {
+            label: "Sessions",
+        },
+        active: {
+            label: "Active",
+            color: "hsl(var(--chart-1-1))",
+        },
+        revoked: {
+            label: "Revoked",
+            color: "hsl(var(--chart-2-2))",
+        },
+    } satisfies ChartConfig
+
+
+    // Define an interface for the device counts
+    interface CountObject {
+        [key: string]: number;
+    }
+
+    // Initialize the device counts object with the correct type
+    const deviceCounts: CountObject = (overview ?? {
+        device_types: [],
+    }).device_types.reduce((acc, device) => {
+        acc[device] = (acc[device] || 0) + 1;
+        return acc;
+    }, {} as CountObject);
+
+    // Count occurrences in the browsers array
+    const browserCounts: CountObject = (overview ?? {
+        browser_types: [],
+    }).browser_types.reduce((acc, browser) => {
+        acc[browser] = (acc[browser] || 0) + 1;
+        return acc;
+    }, {} as CountObject);
+
+    // Count occurrences in the OS types array
+    const osTypeCounts: CountObject = (overview ?? {
+        os_types: [],
+    }).os_types.reduce((acc, os) => {
+        acc[os] = (acc[os] || 0) + 1;
+        return acc;
+    }, {} as CountObject);
+
+    // Define a function to generate colors dynamically
+    const generateColor = (index: number, themeNo: number) => {
+        return `hsl(var(--chart-${index + 1}-${themeNo}))`;
+    };
+
+    // Generic function to generate chart configuration
+    const generateChartConfig = (counts: CountObject, themeNo: number) => {
+        const config: { [key: string]: { label: string; color: string } } = {};
+        const types = Object.keys(counts);
+
+
+        types.forEach((type, index) => {
+            config[type] = {
+                label: type,
+                color: generateColor(index, themeNo),
+            };
+        });
+
+        return config;
+    };
+
+    const sessionDeviceChartData = Object.keys(deviceCounts).map(device => ({
+        name: device,
+        count: deviceCounts[device] || 0,
+        fill: `var(--color-${device})`
+    }));
+
+    const sessionBrowserChartData = Object.keys(browserCounts).map(browser => ({
+        name: browser,
+        count: browserCounts[browser] || 0,
+        fill: `var(--color-${browser})`
+    }));
+
+    const sessionOsTypeChartData = Object.keys(osTypeCounts).map(os => ({
+        name: os,
+        count: osTypeCounts[os] || 0,
+        fill: `var(--color-${os})`
+    }));
+
+    const sessionDeviceChartConfig = generateChartConfig(deviceCounts, 2);
+
+    const sessionBrowserChartConfig = generateChartConfig(browserCounts, 1);
+
+    const sessionOSChartConfig = generateChartConfig(osTypeCounts, 2);
+
+
+    const getOverview = async () => {
         try {
             setLoading(true)
-            const res = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/user/get-all`, {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT}/api/overview/get-all`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -19,7 +137,7 @@ const Overview = () => {
                 cache: 'no-cache',
             });
             const { data } = await res.json();
-            setUsers(data);
+            setOverview(data);
         } catch (error) {
             console.error('Error during POST request:', error);
         }
@@ -27,8 +145,9 @@ const Overview = () => {
     }
 
     useEffect(() => {
-        getAllUsers()
+        getOverview()
     }, [])
+
     return (
         <div>
             {
@@ -37,25 +156,50 @@ const Overview = () => {
                         <Loader />
                     </div>
                     :
-                    <div className='flex justify-center items-center h-[calc(100vh-10rem)]'>
-                        <div className='flex flex-col items-center gap-5'>
-                            <MdOutlineConstruction className='text-primary' size={160} />
-                            <h1 className='text-3xl'>Hold up! Work is in progress</h1>
-                        </div>
-
-                        {/* <div className='grid grid-cols-4 gap-5'>
-                            <Card>
+                    <div>
+                        <div className='grid grid-cols-3 gap-5'>
+                            <DonutChartStats
+                                title='Total Users'
+                                chartData={userChartData}
+                                chartConfig={userChartConfig}
+                                key='name'
+                            />
+                            <Card className='flex flex-col justify-between'>
                                 <CardHeader>
-                                    <CardTitle>Total Users</CardTitle>
+                                    <CardTitle>Blocked Users</CardTitle>
                                 </CardHeader>
-                                <CardContent className='flex justify-between items-end'>
-                                    <p className="text-6xl font-bold">
-                                        {users.length}
+                                <CardContent className='flex flex-col gap-5 items-center'>
+                                    <p className="text-5xl font-bold">
+                                        {overview?.blocked_user_count}
                                     </p>
-                                    <FaUsers size={80} className='text-primary' />
+                                    <FaUsersSlash size={80} className='text-gray-300' />
                                 </CardContent>
                             </Card>
-                        </div> */}
+                            <DonutChartStats
+                                title='Total Sessions'
+                                chartData={sessionChartData}
+                                chartConfig={sessionChartConfig}
+                                key='name'
+                            />
+                            <ChartPie
+                                title='All Devices'
+                                chartData={sessionDeviceChartData}
+                                chartConfig={sessionDeviceChartConfig}
+                                key='name'
+                            />
+                            <ChartPie
+                                title='All Browsers'
+                                chartData={sessionBrowserChartData}
+                                chartConfig={sessionBrowserChartConfig}
+                                key='name'
+                            />
+                            <ChartPie
+                                title='Operating Systems'
+                                chartData={sessionOsTypeChartData}
+                                chartConfig={sessionOSChartConfig}
+                                key='name'
+                            />
+                        </div>
                     </div>
             }
         </div>
