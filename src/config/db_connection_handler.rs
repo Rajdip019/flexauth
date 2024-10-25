6b335b92186f0d245029147a69695b19584fe57f
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2024-10-25 23:45:33 +0530

    add:init local setup

--- src/config/db_connection_handler.rs
+++ src/config/db_connection_handler.rs
@@ -7,16 +7,25 @@ use std::error::Error;
 
 pub async fn connect() -> Result<Client, Box<dyn Error>> {
     // Load the MongoDB connection string from an environment variable:
-    let client_uri = env::var("MONGO_URI").unwrap_or(
-        "mongodb://admin:admin@localhost:27017/?directConnection=true&retryWrites=true&w=majority"
-            .to_string(),
+    let client_uri_main = env::var("MONGO_URI").unwrap_or("localhost".to_string());
+
+    let client_uri = format!(
+        "mongodb://{}:{}@{}:27017/?directConnection=true&retryWrites=true&w=majority",
+        env::var("MONGO_INITDB_ROOT_USERNAME").expect("MONGO_INITDB_ROOT_USERNAME required"),
+        env::var("MONGO_INITDB_ROOT_PASSWORD").expect("MONGO_INITDB_ROOT_PASSWORD required"),
+        client_uri_main
     );
 
-    // A Client is needed to connect to MongoDB:
-    // An extra line of code to work around a DNS issue on Windows:
     let options =
-        ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
-            .await?;
+        match ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare())
+            .await
+        {
+            Ok(options) => options,
+            Err(e) => {
+                eprintln!(">> Error parsing client options: {:?}", e);
+                std::process::exit(1);
+            }
+        };
 
     let client = Client::with_options(options.clone())?;
     // Print success message if the connection is successful or an error message if it fails:
